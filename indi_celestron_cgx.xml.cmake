9656c0c85eaa2d7f36a2a06d3fe55f97c3896c11
Rick Bassham <brodrick.bassham@gmail.com>
2021-01-03 13:25:33 +0000

    build: cleanup of cmake files

--- indi_celestron_cgx.xml.cmake
+++ indi_celestron_cgx.xml.cmake
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
-<devGroup group="Telescopes">
-   <device label="Celestron CGX" manufacturer="Celestron">
-      <driver name="Celestron CGX">indi_celestron_cgx</driver>
-      <version>@CCGX_VERSION_MAJOR@.@CCGX_VERSION_MINOR@</version>
-   </device>
-   <device label="Celestron WiFi" manufacturer="Celestron">
-      <driver name="Celestron CGX">indi_celestron_cgx</driver>
-      <version>@CCGX_VERSION_MAJOR@.@CCGX_VERSION_MINOR@</version>
-   </device>
-</devGroup>
+   <devGroup group="Telescopes">
+      <device label="Celestron CGX" manufacturer="Celestron">
+         <driver name="Celestron CGX">indi_celestron_cgx</driver>
+         <version>@CCGX_VERSION_MAJOR@.@CCGX_VERSION_MINOR@</version>
+      </device>
+      <device label="Celestron WiFi" manufacturer="Celestron">
+         <driver name="Celestron CGX">indi_celestron_cgx</driver>
+         <version>@CCGX_VERSION_MAJOR@.@CCGX_VERSION_MINOR@</version>
+      </device>
+   </devGroup>
 </driversList>
